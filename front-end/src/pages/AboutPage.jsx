4e65117b8b37aaa3a18ec473237d8706f6eb6013
Vladimirs <vladimirribakov123@mail.ru>
2023-01-20 20:52:27 +0000

    installed scss and added style module for pages

--- front-end/src/pages/AboutPage.jsx
+++ front-end/src/pages/AboutPage.jsx
@@ -1,5 +1,7 @@
+import style from './AboutPage.module.scss';
+
 const AboutPage = () => {
-  return <h1>About Page</h1>;
+  return <h1 className={style.app}>About Page</h1>;
 };
 
 export default AboutPage;
--- front-end/src/pages/CarsListPage.jsx
+++ front-end/src/pages/CarsListPage.jsx
@@ -1,5 +1,7 @@
+import style from './CarsListPage.module.scss';
+
 const CarsListPage = () => {
-  return <h1>Cars List Page</h1>;
+  return <h1 className={style.app}>Cars List Page</h1>;
 };
 
 export default CarsListPage;
