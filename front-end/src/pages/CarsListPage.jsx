11a0da39d4d864856f8381396a9ba19958f2cd58
Vladimirs <vladimirribakov123@mail.ru>
2023-01-27 22:11:27 +0000

    making cars list component reusable

--- front-end/src/pages/CarsListPage.jsx
+++ front-end/src/pages/CarsListPage.jsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
 import style from './CarsListPage.module.scss';
 import cars from './car-content';
+import CarsList from '../components/CarsList';
 
 const CarsListPage = () => {
   return (
@@ -8,20 +8,7 @@ const CarsListPage = () => {
       <main className={style.main}>
         <h1>Cars</h1>
         <div className={style.container}>
-          {cars.map((car) => (
-            <Link to={`/cars/${car.Name}`} key={car.id}>
-              <div className={style.card}>
-                <h3>{car.title}</h3>
-                <p>{car.Year}</p>
-                <p>{car.Origin}</p>
-                <img
-                  src={car.Image}
-                  alt={car.title}
-                  style={{ width: '200px' }}
-                />
-              </div>
-            </Link>
-          ))}
+          <CarsList cars={cars} style={style} />
         </div>
       </main>
     </div>
