80444bb368e928ee9760399dbad2b1add1077a48
Vladimirs <vladimirribakov123@mail.ru>
2023-01-30 10:52:03 +0000

    created privacy and policy for the compnay text + working out the back-end server

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 //initializing the express var
 const app = express();
+app.use(express.json());
 
-app.get('/hello', (req, res) => {
-  res.send('Hi!');
+app.post('/hello', (req, res) => {
+  console.log(req.body);
+  res.send(`Hi ${req.body.name} !`);
 });
 
 app.listen(8000, () => {
