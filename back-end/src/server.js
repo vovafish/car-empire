78ced8fa08d8d2c2267cf7bf99596b38fbed22a7
Vladimirs <vladimirribakov123@mail.ru>
2023-02-01 22:06:42 +0000

    adding mongo to the express

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,12 +1,27 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
 
 //initializing the express var
 const app = express();
 app.use(express.json());
 
-app.post('/hello', (req, res) => {
-  console.log(req.body);
-  res.send(`Hi ${req.body.name} !`);
+app.get(`/api/cars/:name`, async (req, res) => {
+  const { name } = req.params;
+
+  const client = new MongoClient(`mongodb://127.0.0.1:27017`);
+  await client.connect();
+
+  const db = client.db(`car-empire`);
+
+  const car = await db.collection('cars').findOne({ name });
+
+  if (car) {
+    res.json(car);
+  } else {
+    res.sendStatus(404);
+  }
+
+  res.json(car);
 });
 
 app.listen(8000, () => {
