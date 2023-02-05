1f91a287dadfa6288c720bdef54b9fcd558d6c33
Vladimirs <vladimirribakov123@mail.ru>
2023-02-05 21:23:35 +0000

    working on the system but stuck on a issue with the server

--- front-end/src/pages/CarPage.jsx
+++ front-end/src/pages/CarPage.jsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import cars from './car-content';
 import NotFoundPage from './NotFoundPage';
 //import style from './CarPage.module.scss';
 
 const CarPage = () => {
+  const [carInfo, setCarInfo] = useState({
+    name: 'Text',
+    Year: '0000',
+  });
+
   const { carId } = useParams();
+
+  useEffect(() => {
+    const loadCarInfo = async () => {
+      const response = await axios.get(`/api/cars/${carId}`);
+      const newCarInfo = response.data;
+      setCarInfo(newCarInfo);
+    };
+    loadCarInfo();
+  }, [carId]);
+
   const car = cars.find((car) => car.Name === carId);
 
   if (!car) {
@@ -14,6 +31,9 @@ const CarPage = () => {
   return (
     <>
       <h1>{car.title}</h1>
+      <p>
+        This is {carInfo.name} and been created in {carInfo.Year}
+      </p>
       <div>
         <p>It uses {car.Miles_per_Gallon} miles per gallon</p>
         <p>Contains {car.Cylinders} cylinders</p>
