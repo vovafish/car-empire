d70e3e37ff8fcc1336d71f2d7c23d992b764d7b2
Vladimirs <vladimirribakov123@mail.ru>
2023-02-19 19:52:50 +0000

    implementing JWT on the front-end

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -90,6 +90,37 @@ app.post('/api/signup', async (req, res) => {
   );
 });
 
+app.post('/api/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await db.collection('users').findOne({ email });
+
+  if (!user) res.sendStatus(401);
+
+  const { _id: id, isVerified, passwordHash } = user;
+
+  const isCorrect = await bcrypt.compare(password, passwordHash);
+
+  if (isCorrect) {
+    jwt.sign(
+      { id, isVerified, email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '2d',
+      },
+      (err, token) => {
+        if (err) {
+          res.status(500).json(err);
+        }
+
+        res.status(200).json({ token });
+      }
+    );
+  } else {
+    res.sendStatus(401);
+  }
+});
+
 const PORT = process.env.PORT || 8000;
 
 /* If connection if successful to the database then output releated message and say what port running on */
